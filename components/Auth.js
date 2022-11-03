684c01bcfe094e4f2ff1a71c90b41cbe53283e22
Benedict <benedictryan80@gmail.com>
2022-11-03 16:20:05 +0700

    set custom dynamic background color and add code splitting

--- components/Auth.js
+++ components/Auth.js
@@ -1,9 +1,10 @@
-import React from 'react';
 import Link from 'next/link';
-import GoogleLogIn from './GoogleLogIn';
 import AuthLabel from './AuthLabel';
+import dynamic from 'next/dynamic';
+const AuthButton = dynamic(() => import('./AuthButton'));
+const GoogleLogIn = dynamic(() => import('./GoogleLogIn'));
 
-function Auth({ login, register }) {
+function Auth({ register }) {
 	return (
 		<div className="md:flex lg:flex   items-center justify-center  py-8 mx-auto mt-10 md:mt-20 lg:mt-20 lg:py-0">
 			<div className="w-full bg-white rounded-lg shadow border md:mt-0  sm:max-w-md dark:bg-[#2D3748] dark:border-gray-700  border-gray-200">
@@ -20,12 +21,7 @@ function Auth({ login, register }) {
 						)}
 						<AuthLabel name="email" title="Email Address" />
 						<AuthLabel name="password" title="Password" />
-						<button
-							type="submit"
-							className="w-full text-white bg-blue-600  hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-md px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-						>
-							{register ? 'Sign up' : 'Sign in'}
-						</button>
+						<AuthButton register={register} />
 						<p className="text-md font-light text-gray-500 dark:text-gray-400">
 							{register
 								? 'Already have an account? '
---
+++ components/AuthButton.js
@@ -0,0 +1,12 @@
+function AuthButton({ register }) {
+	return (
+		<button
+			type="submit"
+			className="w-full text-white bg-blue-600  hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-md px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+		>
+			{register ? 'Sign up' : 'Sign in'}
+		</button>
+	);
+}
+
+export default AuthButton;
