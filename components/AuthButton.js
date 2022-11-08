396175ef9f64c0f3a4534b14911a44ce8aaafe6b
Benedict <benedictryan80@gmail.com>
2022-11-09 00:25:04 +0700

    add Loading info

--- components/AuthButton.js
+++ components/AuthButton.js
@@ -80,7 +80,7 @@ function AuthButton({ register }) {
 					onClick={handleSignUp}
 					className="w-full text-white bg-blue-600  hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-md px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
 				>
-					Sign up
+					{isLoading ? 'Loading...' : 'Sign up'}
 				</button>
 			) : (
 				<button
@@ -88,7 +88,7 @@ function AuthButton({ register }) {
 					onClick={handleLogIn}
 					className="w-full text-white bg-blue-600  hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-md px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
 				>
-					Sign in
+					{isLoading ? 'Loading...' : 'Sign in'}
 				</button>
 			)}
 			<Toaster
